116d07b68859c83da180556af098cd2b6257415f
Wolfgang Schwendtbauer <wolfgang.schwendtbauer@gmail.com>
2022-08-03 14:03:49 +0200

    remove unneeded code

--- angry-bot/src/helpers/message-wrapper.ts
+++ angry-bot/src/helpers/message-wrapper.ts
@@ -5,16 +5,11 @@ export type PluginReturnCode = "CONTINUE" | "DELETED" | "ABORT";
 
 export class MessageWrapper {
     isDeleted = false;
-    isAborted = false;
 
-    role: Role;
-
-    constructor(private message: Message) {
-        this.role = Role.OWNER;
-    }
+    constructor(private message: Message) {}
 
     async applyPlugin(plugin: (message: Message) => Promise<PluginReturnCode>) {
-        if (this.isDeleted || this.isAborted) {
+        if (this.isDeleted) {
             return;
         }
 
@@ -22,8 +17,6 @@ export class MessageWrapper {
 
         switch (code) {
             case "ABORT":
-                this.isAborted = true;
-                break;
             case "DELETED":
                 this.isDeleted = true;
                 break;
