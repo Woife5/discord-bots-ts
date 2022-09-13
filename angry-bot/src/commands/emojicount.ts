5e266afeed7e451d5ce69230a92c9fbb5c5bcfda
Wolfgang Schwendtbauer <wolfgang.schwendtbauer@gmail.com>
2022-09-13 17:30:52 +0200

    fix emojicount command

--- angry-bot/src/commands/emojicount.ts
+++ angry-bot/src/commands/emojicount.ts
@@ -2,11 +2,14 @@ import { CommandInteraction, Message, MessageEmbed, User as DiscordUser } from "
 import { SlashCommandBuilder } from "@discordjs/builders";
 import { Stats, User } from "@helpers";
 import { ICommand } from "./command-interfaces";
+import { angryEmojis } from "@data";
 
 const embedColor = "#d94d26";
 
-async function runCommand(user?: DiscordUser) {
+async function runCommand(user: DiscordUser | null | undefined) {
     let emojicount = 0;
+
+    const embed = new MessageEmbed().setColor(embedColor).setTitle("Emoji Count");
 
     if (user) {
         const userResult = await User.findOne({ userId: user.id }).exec();
@@ -16,6 +19,16 @@ async function runCommand(user?: DiscordUser) {
         }
 
         emojicount = Object.values(userResult.emojis).reduce((acc, val) => acc + val, 0);
+
+        // Get top 5 used emojis
+        const topEmojis = Object.entries(userResult.emojis)
+            .sort((a, b) => b[1] - a[1])
+            .slice(0, 5)
+            .map(([id, count]) => `${angryEmojis[parseInt(id)]} - ${count}x`)
+            .join("\n");
+
+        embed.setDescription(topEmojis);
+        embed.setTitle(`Top 5 emojis sent for ${user.username}`);
     } else {
         const val = await Stats.findOne({ key: "total-angry-emojis-sent" }).exec();
 
@@ -24,20 +37,24 @@ async function runCommand(user?: DiscordUser) {
         }
 
         emojicount = val.value;
+
+        embed.addField("Total angry emojis sent", emojicount.toString());
     }
 
-    return new MessageEmbed()
-        .setColor(embedColor)
-        .setTitle("Emoji Stats")
-        .addField(`${user ? "Your t" : "T"}otal emoji cout`, `${emojicount}`);
+    return embed;
 }
 
 export const emojicount: ICommand = {
-    data: new SlashCommandBuilder().setName("emojicount").setDescription("Get the total number of angry emojis sent."),
+    data: new SlashCommandBuilder()
+        .setName("emojicount")
+        .setDescription("Get the total number of angry emojis sent.")
+        .addUserOption(option => option.setName("user").setDescription("The user to get the emoji count for.")),
     executeInteraction: async (interaction: CommandInteraction): Promise<void> => {
-        interaction.reply({ embeds: [await runCommand(interaction.user)] });
+        const user = interaction.options.getUser("user");
+        interaction.reply({ embeds: [await runCommand(user)] });
     },
     executeMessage: async (message: Message): Promise<void> => {
-        message.reply({ embeds: [await runCommand()] });
+        const user = message.mentions.users.first();
+        message.reply({ embeds: [await runCommand(user)] });
     },
 };
