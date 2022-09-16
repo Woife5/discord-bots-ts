55263b06fa419f72040c0af355d10575edcc1661
Wolfgang Schwendtbauer <wolfgang.schwendtbauer@gmail.com>
2022-09-16 16:24:59 +0200

    fix tarot 92 and update dev-dependencies

--- angry-bot/src/data/angry-tarot.ts
+++ angry-bot/src/data/angry-tarot.ts
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import { ITarot } from "../commands";
+import { ITarot } from "../commands/command-interfaces";
 
 export const tarots: ITarot[] = [
     {
@@ -266,7 +266,7 @@ export const tarots: ITarot[] = [
         text: ":angry91: wird dir deinen Tag zerstören. Wegen ihm wirst du den ganzen Tag lang ein Lächeln im Gesicht haben und Gefühle von Freude spüren. Nichts kann dich heute wütend machen. ",
     },
     {
-        text: "What the fuck did you just fucking say about me, you little bitch? I'll have you know I graduated top of my class in the Navy Seals and I've been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in guerilla warfare and I'm the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You're fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that's just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little \"clever\" comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn't, you didn't, and now you're paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You're fucking dead, kiddo.",
+        text: "What the fuck did you just fucking say about me, you little bitch? I'll have you know I graduated top of my class in the Navy Seals and I've been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in guerilla warfare and I'm the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You're fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that's just with my bare hands. Not only am I extensively trained in unarmed combat...",
     },
     {
         text: ":angry93: steht für den Moment, als du dir damals in der Volkschule in die Hose gemacht hast.",
