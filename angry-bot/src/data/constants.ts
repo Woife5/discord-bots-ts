4baad7f2a4fa2ad705e2413635945d2d8bc8e767
Wolfgang Schwendtbauer <wolfgang.schwendtbauer@gmail.com>
2022-09-29 09:22:20 +0200

    update build process

--- angry-bot/src/data/constants.ts
+++ angry-bot/src/data/constants.ts
@@ -1,7 +1,10 @@
-export const version = "2.8.7";
 export const prefix = "?angry";
 export const angryBirthday = new Date("2021-03-24T00:00:00.000Z");
 export const repoURL = "https://github.com/Woife5/discord-bots-ts";
 export const angryIconCDN = "https://cdn.discordapp.com/attachments/314440449731592192/912125148474245221/angry.png";
 export const angryReactionsAmount = 5;
 export const adminRoleId = "824234599936557097";
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+export { version } from "../../package.json";
