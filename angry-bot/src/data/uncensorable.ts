550e56b43e1061f7741028f8a57d6fced7bf35e3
Tintifax <64472799+Tintifax69@users.noreply.github.com>
2022-12-26 12:35:29 +0100

    hopefully fix another oversight

--- angry-bot/src/data/uncensorable.ts
+++ angry-bot/src/data/uncensorable.ts
@@ -11,6 +11,7 @@ export const uncensorable = [
     /^.?angry.?$/,
     /^:angry32:$/,
     /^(((<?:)?a)?n)?gry[0-9]?$/, //prevent censoring all or multible angrys at once
+    /^<?:[^:]{0,5}$/, //hopefully prevet abusing <:emoji and :emoji style expressions 
     /^🤮$/,
     /^(.{0,2}\.)?png$/,
     /^attachments$/, //idk what this does, was in the list
