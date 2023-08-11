99de0815b259174e2d843a3623b42a74327415a8
Wolfgang Schwendtbauer <wolfgang.schwendtbauer@gmail.com>
2023-08-12 00:20:01 +0200

    [willhaben] finalize search command

--- willhaben-bot/src/commands/search.ts
+++ willhaben-bot/src/commands/search.ts
@@ -1,4 +1,11 @@
-import { EmbedBuilder, SlashCommandBuilder } from "discord.js";
+import {
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+    ComponentType,
+    EmbedBuilder,
+    SlashCommandBuilder,
+} from "discord.js";
 import { CommandHandler } from "@woife5/shared/lib/commands/types";
 import willhaben from "willhaben";
 
@@ -26,8 +33,8 @@ type WillhabenResult = {
     enddate: number;
 };
 
-console.log(willhaben.new().keyword("test").getURL());
-process.exit(0);
+const PAGE_SIZE = 5;
+const PAGE_AMOUNT = 5;
 
 function isCategory(category: string): boolean {
     return !!willhaben.getCategories[category];
@@ -47,24 +54,80 @@ export const search: CommandHandler = {
         const searchTerm = interaction.options.getString("searchterm", true);
         const category = interaction.options.getString("category", false);
 
+        await interaction.deferReply();
+
         if (category && !isCategory(category)) {
-            await interaction.reply({
-                embeds: [defaultEmbed().setDescription("Invalid category. Please select a valid one from here: ")],
+            await interaction.editReply({
+                embeds: [
+                    defaultEmbed()
+                        .setTitle("Unknown category x.x")
+                        .setDescription(
+                            "Please select one from this list: https://gist.github.com/Woife5/00405ef11eb624ccd70a49cc3ffe480d"
+                        ),
+                ],
             });
             return;
         }
 
-        const results: WillhabenResult[] = await willhaben.new().keyword(searchTerm).count(5).search();
+        let page = 1;
+        const results: WillhabenResult[] = await willhaben
+            .new()
+            .keyword(searchTerm)
+            .count(PAGE_SIZE * PAGE_AMOUNT)
+            .search();
+
         const embed = defaultEmbed().addFields(
-            results.map(res => ({
+            results.slice(0, PAGE_SIZE).map(res => ({
                 name: res.heading.substring(0, 100),
                 value: res.body_dyn.substring(0, 2000),
             }))
         );
 
-        await interaction.reply({ embeds: [embed] });
+        if (!interaction.channel) {
+            await interaction.editReply({ embeds: [embed] });
+            return;
+        }
+
+        const nextButton = new ButtonBuilder().setCustomId("next").setLabel("➡️").setStyle(ButtonStyle.Primary);
+        const prevButton = new ButtonBuilder().setCustomId("prev").setLabel("⬅️").setStyle(ButtonStyle.Primary);
+        const buttonRow = new ActionRowBuilder<ButtonBuilder>().addComponents([prevButton, nextButton]);
+        const collector = interaction.channel.createMessageComponentCollector({
+            filter: i => i.user.id === interaction.user.id,
+            componentType: ComponentType.Button,
+            time: 60_000,
+        });
+
+        collector.on("collect", async i => {
+            if (i.customId === "next" && page < PAGE_AMOUNT) {
+                page += 1;
+            }
+
+            if (i.customId === "prev" && page > 1) {
+                page -= 1;
+            }
+
+            await i.update({ embeds: [buildEmbed(results, page)] });
+        });
+
+        collector.on("end", async () => {
+            const expired = defaultEmbed().setDescription("This search has expired.");
+            await interaction.editReply({ embeds: [expired], components: [] });
+        });
+
+        await interaction.editReply({ embeds: [buildEmbed(results, page)], components: [buttonRow] });
     },
 };
+
+function buildEmbed(results: WillhabenResult[], page: number) {
+    return defaultEmbed()
+        .addFields(
+            results.slice((page - 1) * PAGE_SIZE, page * PAGE_SIZE).map(res => ({
+                name: res.heading.substring(0, 100),
+                value: res.body_dyn.substring(0, 2000),
+            }))
+        )
+        .setFooter({ text: `Page ${page}/${PAGE_AMOUNT}` });
+}
 
 function defaultEmbed() {
     return new EmbedBuilder().setAuthor({
